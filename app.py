ee638fea375943aec700217e5e0eb58d749d3a6a
Jon Skinner <jon.a.skinner1@gmail.com>
2023-02-05 15:47:29 -0500

    remove none on home page default

--- app.py
+++ app.py
@@ -10,20 +10,21 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/", methods=("GET", "POST"))
 def index():
-	if request.method == "POST":
-		plant = request.form["plant"]
-		response = openai.Completion.create(
-			model="text-davinci-003",
-			prompt=generate_prompt(plant),
-			max_tokens=1024,
-			n=1,
-			stop=None,
-			temperature=0.5
-		)
-		return redirect(url_for("index", result=response.choices[0].text))
+    if request.method == "POST":
+        plant = request.form["plant"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(plant),
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
 
-	result = request.args.get("result")
-	return render_template("index.html", result=Markup(result))
+    result = request.args.get("result")
+    result = result if result else ""
+    return render_template("index.html", result=Markup(result))
     
 def generate_prompt(plant):
     return "Tell me how to care for a {} plant, which three are very similar in care instructions? send the answer in html format so it's easy to read please.".format(plant)
