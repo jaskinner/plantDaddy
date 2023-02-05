2e2d658adaa3336d79dac87f774b0fb0b38cf3b6
Jon Skinner <jon.a.skinner1@gmail.com>
2023-02-05 14:25:43 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import os, requests
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for, Markup
+
+# def create_app(*args, **kwargs):
+app = Flask(__name__)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+	if request.method == "POST":
+		plant = request.form["plant"]
+		response = openai.Completion.create(
+			model="text-davinci-003",
+			prompt=generate_prompt(plant),
+			max_tokens=1024,
+			n=1,
+			stop=None,
+			temperature=0.5
+		)
+		return redirect(url_for("index", result=response.choices[0].text))
+
+	result = request.args.get("result")
+	return render_template("index.html", result=Markup(result))
+#     return app
+    
+def generate_prompt(plant):
+    url = "http://captainbonnysplantery.com/product-category/allplants/"
+    response = requests.get(url)
+
+    print(response)
+
+    return "Tell me how to care for a {} plant, which three are very similar in care instructions? send the answer in html format so it's easy to read please.".format(plant)
